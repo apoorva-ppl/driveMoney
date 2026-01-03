065e3dca7103a7a7ba2a02da5f9fc24a5f259a26
Apoorva Pandey <leadapu0101@gmail.com>
2026-01-03 15:29:59 +0530

    fixing

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,32 +2,41 @@ import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
 import AuthRoutes from "./routes/AuthRoutes.js";
-import cookieParser from "cookie-parser";
 import TransactionRoutes from "./routes/TransactionRoutes.js";
+import cookieParser from "cookie-parser";
 import { apiLimiter } from "./middleware/rateLimiter.js";
 import helmet from "helmet";
 import cors from "cors";
+
 dotenv.config();
 connectDB();
 
 const app = express();
-app.use(express.json());
 
-app.use("/api/auth", AuthRoutes);
-app.use(cookieParser());
-app.use("/api/transactions", TransactionRoutes);
-app.use("/api", apiLimiter);
+
 app.use(helmet());
+app.use(express.json());
+app.use(cookieParser());
+
 app.use(
   cors({
-    origin: [
-      "http://localhost:5173",
-      "https://YOUR_FRONTEND_URL.vercel.app",
-    ],
+    origin: "*", 
     credentials: true,
   })
 );
+
+app.use("/api", apiLimiter);
+
+app.use("/api/auth", AuthRoutes);
+app.use("/api/transactions", TransactionRoutes);
+
+app.get("/", (req, res) => {
+  res.send("DriveMoney backend is running 🚀");
+});
+
+
 const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
